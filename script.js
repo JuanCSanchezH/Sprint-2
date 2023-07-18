2a3aeb53e2025d635287b0ded5fb022b03f559aa
Juan Camilo Sánchez <juan.sanchez53@udea.edu.co>
2023-07-18 07:30:23 -0500

    shopping cart commit

--- script.js
+++ script.js
@@ -1,52 +1,14 @@
-// function addItemToCart(image-product-1) {
-//     // Add the product to the cart.
-//     cart.addProduct(image-pro);
-//     // Update the cart total.
-//     cartTotal = cart.getTotalPrice();
-//     // Update the number of items in the cart.
-//     numberOfItems = cart.getNumberOfItems();
-//     // Update the UI.
-//     updateCartUI();
-//   }
-//   function removeItemFromCart(productId) {
-//     // Remove the product from the cart.
-//     cart.removeProduct(productId);
-//     // Update the cart total.
-//     cartTotal = cart.getTotalPrice();
-//     // Update the number of items in the cart.
-//     numberOfItems = cart.getNumberOfItems();
-//     // Update the UI.
-//     updateCartUI();
-//   }
-//   function updateCartUI() {
-//     // Update the number of items in the cart.
-//     document.getElementById("numberOfItems").innerHTML = numberOfItems;
-//     // Update the cart total.
-//     document.getElementById("cartTotal").innerHTML = cartTotal;
-//   }
-
-// var mainImage = document.querySelector("#image-product-1");
-// var thumbnails = document.querySelectorAll(".Thumbnails img");
-// for (var i = 0; i < thumbnails.length; i++) {
-//   thumbnails[i].addEventListener("click", function(event) {
-//     mainImage.src = this.src;
-//   });
-// }
-
-// document.addEventListener("add-to-cart", function(event) {
-//     // Get the product ID from the event.
-//     var productId = event.detail.productId;
-//     // Add the product to the cart.
-//     cart.addProduct(productId);
-//   });
-
 import {listProducts, createProduct, updateProduct, deleteProduct} from './services/api.js';
 
 let arrProductos = [];
 const formulario = document.querySelector("form");
 const updateProd = document.querySelector("#updateProduct")
 let editButton = null;
+let pCount = null;
 let idSelectedProduct = null;
+let cart = [];
+let total = null;
+let img = null;
 
 // updateProd.addEventListener('click', async()=>{
 //     const formdata = new FormData(formulario);
@@ -75,60 +37,200 @@ document.addEventListener('click', (event) =>  {
         idSelectedProduct = event.target.attributes['data-id'].value;
         deleteProduct(idSelectedProduct)
     }
-})
 
+    if(event.target.classList.contains("counter-buttons")){
+        console.log('Hola')
+        idSelectedProduct = event.target.attributes['data-id'].value;
+        const operacion = event.target.attributes["value"].nodeValue;
+        const product = arrProductos.find(item => item.ID == idSelectedProduct);
+        pCount.forEach(item =>{
+            item.textContent = '';
+            if(idSelectedProduct==item.attributes['data-id'].value){
+                if(operacion == "+"){
+                    console.log(product.contador);
+                    product.contador += 1;
+                }else if(operacion == "-" && product.contador>1){
+                    console.log(product.contador);
+                    product.contador -= 1;
+                }
+                item.textContent = product.contador;
+            }
+        })
+    }
+
+    if(event.target.classList.contains("addToCart")){
+        $("[data-bs-toggle='popover']").popover('show');
+        let html = document.getElementById("contenido");
+        html.innerHTML = ``;
+        idSelectedProduct = event.target.attributes['data-id'].value;
+        const product = arrProductos.find(item => item.ID == idSelectedProduct)
+        swal({
+            title: product.nombre,
+            type: "success",
+            text: "This product has been added to your cart.",
+            showConfirmButton: false,
+            timer: 2500
+        });
+        if(cart.includes(product)){
+            product.unidades += product.contador;
+        }else{
+            product.unidades = product.contador; 
+            cart.push(product);
+        }
+        total = 0;
+        cart.forEach(item => {
+            total += item.precio*item.unidades;
+            html.innerHTML += `
+            <div class="row mb-2">
+                <div class="col-5">
+                    <img src=${item.imagenPrincipal[0]} alt="">
+                </div>
+                <div class="col-7">
+                    <h6><b>${item.nombre}</b></h6>
+                    <div class="row">
+                        <p class="col-6" id="price-popover">$${item.precio}</p>
+                        <p class="col-6" id="discount-popover">${item.descuento}% off</p>
+                    </div>
+                    <p id="units-popover">Units: ${item.unidades}</p>
+                </div>
+            </div>
+            `
+        });
+        html.innerHTML += `
+            <div class="row mb-2">
+                <p class="col-12" id="price-popover">Total: $${total}</p>
+            </div>
+            <!-- Button trigger modal -->
+            <button id="button-buy-now" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Buy now
+            </button>
+        `
+        const btnBuy = document.getElementById("button-buy-now");    
+        btnBuy.onclick = function(){
+            $('#exampleModal2').modal('hide');
+            $('body').removeClass('modal-open');
+            $('.modal-backdrop').remove();
+        }
+        if(cart.length > 0){
+            const p = document.getElementById("p-cart");
+            p.textContent = cart.length;
+        }
+    }
+
+    if(event.target.classList.contains("mainImage")){
+        $('#exampleModal').modal('hide');
+        $('body').removeClass('modal-open');
+        $('.modal-backdrop').remove();
+    }
+})
 
 const printProductos = async() => {
     arrProductos = await listProducts();
     let html = ``;
-    console.log(arrProductos);
     arrProductos.forEach(item => {
+        const originalPrice = (item.precio/(1-(item.descuento/100))).toFixed(2);
         const li = `
         <section class="py-5 col-md-6">
             <div class="container">
-                <img class="mainImage" id="image-product-1" src="${item.imagenPrincipal}" alt="Imagen principal">
-                <div class="Thumbnails">
-                    <a href="#"><img src="${item.Thumbnails[1]}" alt="Miniatura1" onclick="changeImage('images/product-1-thumbnail.jpg')"></a>
-                    <a href="#"><img src="images/image-product-2-thumbnail.jpg" alt="Miniatura2" onclick="changeImage('images/product-2-thumbnail.jpg')"></a>
-                    <a href="#"><img src="images/image-product-3-thumbnail.jpg" alt="Miniatura3" onclick="changeImage('images/producto-3-thumbnail.jpg')"></a>
-                    <a href="#"><img src="images/image-product-4-thumbnail.jpg" alt="Miniatura4" onclick="changeImage('images/producto-4-thumbnail.jpg')"></a>
+                <a class="mainImage" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal2"><img class="mainImage" id="myImg" src=${item.imagenPrincipal[0]} alt="Imagen principal"></a>
+                <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <div id="carouselExampleFade" class="carousel slide carousel-fade">
+                                <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src=${item.imagenPrincipal[0]} class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src=${item.imagenPrincipal[1]} class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src=${item.imagenPrincipal[2]} class="d-block w-100" alt="...">
+                                </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <div class="Thumbnails">
+                    <a href="#"><img src=${item.Thumbnails[0]} alt="Miniatura1" onclick="changeImage('images/product-1-thumbnail.jpg')"></a>
+                    <a href="#"><img src=${item.Thumbnails[1]} alt="Miniatura2" onclick="changeImage('images/product-2-thumbnail.jpg')"></a>
+                    <a href="#"><img src=${item.Thumbnails[2]} alt="Miniatura3" onclick="changeImage('images/producto-3-thumbnail.jpg')"></a>
+                    <a href="#"><img src=${item.Thumbnails[3]} alt="Miniatura4" onclick="changeImage('images/producto-4-thumbnail.jpg')"></a>
+                </div>
+                <!--<div id="myModal" class="modal">
+                    <span class="close">&times;</span>
+                    <img class="modal-content" id="img01">
+                </div> -->
             </div>
         </section>
         <article class="col-md-6"> 
             <div class="Product-details">
                 <h6>SNEAKER COMPANY</h6>
-                <h1>Fall Limited Edition Sneakers</h1>
-                <p>These low-profile sneakers are you perfect profile wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
+                <h1>${item.nombre}</h1>
+                <p>${item.descripcion}</p>
                 <div class="Price">
-                    <p>$125.00</p>
+                    <p>$${item.precio}</p>
                     <div id="cont50">
-                        <p>50%</p>
+                        <p>${item.descuento}%</p>
                     </div>
                 </div>
-                <s id="descuento">$250.00</s>
+                <s id="descuento">$${originalPrice}</s>
             </div>
             <div class="add-article">
                 <div class="counter">
-                    <button class="counter-buttons">-</button>
-                    <p>1</p>
-                    <button class="counter-buttons">+</button>
+                    <button class="counter-buttons" value="-" data-id="${item.ID}">-</button>
+                    <p class="p-count" data-id="${item.ID}">${item.contador}</p>
+                    <button class="counter-buttons" value="+" data-id="${item.ID}">+</button>
                 </div>
-                <button id="Add-to-cart">
-                <img src="images/shopping-cart.jpg" alt="">
-                Add to cart
+                <button type="button" id="Add-to-cart" class="addToCart" data-id="${item.ID}">
+                    <img src="images/shopping-cart.jpg" alt="">
+                    Add to cart
                 </button>
             </div>
         </article>`
         html += li
     });
-    const ul = document.getElementById('listaProductos')
+    const ul = document.getElementById('listaProductos');
     ul.innerHTML = html;
     editButton = document.querySelector(".editProduct");
+    pCount = document.querySelectorAll(".p-count")
+    pCount.value = 0;
+    //img = document.getElementById("myImg");    
+    // Get the modal
+    // let modal = document.getElementById("myModal");
 
+    // // Get the image and insert it inside the modal - use its "alt" text as a caption
+    // var img = document.getElementById("myImg");
+    // var modalImg = document.getElementById("img01");
+    //     img.onclick = function(){
+    //     modal.style.display = "block";
+    //     modalImg.src = this.src;
+    // }
+
+    // // Get the <span> element that closes the modal
+    // var span = document.getElementsByClassName("close")[0];
+
+    // // When the user clicks on <span> (x), close the modal
+    // span.onclick = function() { 
+    // modal.style.display = "none";
+    // }
+    
 }
-
-
 
 formulario.addEventListener('submit', async (event) => {
     event.preventDefault();
